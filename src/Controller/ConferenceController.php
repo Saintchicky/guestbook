aacfc37e407a545c8aad27544c5d3ecc847f48db
Saintchicky <poussin6494@hotmail.com>
2022-06-16 18:31:27 +0200

    admin

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,10 +15,10 @@ class ConferenceController extends AbstractController
             <<<EOF
             <html>
                 <body>
-                <img src="/images/under-construction.gif" />
-            </body>
+                    <img src="/images/under-construction.gif" />
+                </body>
             </html>
             EOF       
-        );
+        );       
     }
-}
+}       

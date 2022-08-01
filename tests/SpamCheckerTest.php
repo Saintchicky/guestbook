713a22f740b4c4b828c8f0766164c3cbda28d82b
Saintchicky <poussin6494@hotmail.com>
2022-08-01 08:35:38 +0200

    tutot

--- tests/SpamCheckerTest.php
+++ tests/SpamCheckerTest.php
@@ -2,12 +2,49 @@
 
 namespace App\Tests;
 
+use App\SpamChecker;
+use App\Entity\Comment;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Component\HttpClient\Response\MockResponse;
 
 class SpamCheckerTest extends TestCase
 {
-    public function testSomething(): void
+    public function testSpamScoreWithInvalidRequest(): void
     {
-        $this->assertTrue(true);
+        $comment = new Comment();
+        $comment->setCreatedAt();
+        $context = [];
+
+        $client = new MockHttpClient([new MockResponse('invalid', ['response_headers' => ['x-akismet-debug-help: Invalid key']])]);
+        $checker = new SpamChecker($client, 'abcde');
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Unable to check for spam: invalid (Invalid key).');
+        $checker->getSpamScore($comment, $context);
+    }
+    /**
+    * @dataProvider getComments
+    */
+    public function testSpamScore(int $expectedScore, ResponseInterface $response, Comment $comment, array $context)
+    {
+        $client = new MockHttpClient([$response]);
+        $checker = new SpamChecker($client, 'abcde');
+        $score = $checker->getSpamScore($comment, $context);
+        $this->assertSame($expectedScore, $score);
+    }
+
+    public function getComments(): iterable
+    {
+        $comment = new Comment();
+        $comment->setCreatedAtValue();
+        $context = [];
+        $response = new MockResponse('', ['response_headers' => ['x-akismet-pro-tip: discard']]);
+        yield 'blatant_spam' => [2, $response, $comment, $context];
+        $response = new MockResponse('true');
+        yield 'spam' => [1, $response, $comment, $context];
+        $response = new MockResponse('false');
+        yield 'ham' => [0, $response, $comment, $context];
     }
 }
